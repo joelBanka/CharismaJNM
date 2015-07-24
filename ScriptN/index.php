924db875059e6642dac791d48b9a9c504be400d6
MomoRpz <mermozrpz@gmail.com>
2015-07-24 11:45:22 +0200

    login form modify by mermoz

--- ScriptN/index.php
+++ ScriptN/index.php
@@ -8,31 +8,48 @@ $msg3='';
 
 $nom='';
 $prenom='';
+$emptyData= TRUE;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
- if(empty($_POST['nom']))
-{
-$msg1 = "Le Champ Nom doit être rempli";
-}
+    
+   if(isset($_POST['nom']) && trim($_POST['nom'])=="")
+    {
+        $emptyData = TRUE;
+        $msg1 = "Le Champ Nom doit être rempli";      
+    }
+    else {
+        $emptyData = FALSE;      
+    }
+    
+    if(isset($_POST['prenom']) && trim($_POST['prenom']) == "")
+    {
+        $emptyData = TRUE;
+        $msg2 = "Le Champ Prenom doit être rempli";  
+    }
+    else {
+        $emptyData = FALSE;   
+    }
 
-if(empty($_POST['prenom']))
-{
-$msg2 = "Le Champ Prenom doit être rempli";
-}
-
-if(empty($_POST['mdp']))
-{
-$msg3 = "Entrer le mot de pass";
-}
-
-
+    if(isset ($_POST['mdp']) && trim($_POST['mdp'])== "")
+    {
+        $emptyData = TRUE;  
+        $msg3 = "Entrer le mot de pass";
+    }
+    else {
+        $emptyData = FALSE;    
+    }
 } 
 
-
-if (isset($_POST["nom"]) && ($_POST["prenom"]) )
+if (!$emptyData && isGoodPasword($_POST["mdp"]))
+       // isset($_POST["nom"]) && ($_POST["prenom"]))
 {
   $entrer = InsertionNomPrenom($_POST['nom'], $_POST['prenom']);
+    header('Location: title.php');
+  
+}
+else {
+    echo "insertion echoue";
 }
 
 /*$nom = $_POST['nom'];
